df3d0e828f711619fadd6472b394ca2531f04d85
Mateusz Tokarz <mateusztokarz81@gmail.com>
2025-11-08 10:46:16 +0100

    BST.h na podstawie eduinf.waw - nie dokończone

--- BST_Project/BST_Project/BST.h
+++ BST_Project/BST_Project/BST.h
@@ -1,1 +1,32 @@
 #pragma once
+#include <iostream>
+
+
+struct BSTNode {
+	int key;
+	BSTNode* left;
+	BSTNode* right;
+	BSTNode* parent;
+	BSTNode(int k) : key(k), left(nullptr), right(nullptr), parent(nullptr) {}
+
+};
+
+
+class BST {
+public:
+	BST();
+	~BST();
+
+bool insert(int key);
+BSTNode* search(int key);
+BSTNode* minNode(BSTNode* x);
+BSTNode* maxNode(BSTNode* x);
+BSTNode* pred(BSTNode* x);
+BSTNode* succ(BSTNode* x);
+void remove(int key);
+
+void preorder(BSTNode* node);
+void inorder(BSTNode* node);
+void postorder(BSTNode* node);
+void display();
+
