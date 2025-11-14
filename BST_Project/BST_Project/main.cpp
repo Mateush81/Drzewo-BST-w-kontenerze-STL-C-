31dc4a7acc99065f19d1b7225b11bd387ac8497e
dawidpp2004@wp.pl <dawidpp2004@wp.pl>
2025-11-14 16:09:47 +0100

    zmiana main na inny sposób

--- BST_Project/BST_Project/main.cpp
+++ BST_Project/BST_Project/main.cpp
@@ -1,88 +1,106 @@
-﻿#include <iostream>       // Biblioteka do wejścia/wyjścia
-#include <string>         // Biblioteka do używania typu std::string
-#include "BST.h"          // Klasa BST – logika drzewa
-#include "FileManager.h"  // Klasa FileManager – obsługa plików
+﻿#include <iostream>
+#include <string>
+#include "BST.h"
+#include "FileManager.h"
 
 using namespace std;
 
-
-
-// Funkcja wyświetlająca menu programu
-void printMenu() {
-    cout << "\n===== MENU =====" << endl;
-    cout << "1) Dodaj element" << endl;
-    cout << "2) Usun element" << endl;
-    cout << "3) Szukaj drogi do elementu" << endl;
-    cout << "4) Min/Max" << endl;
-    cout << "5) Preorder" << endl;
-    cout << "6) Inorder" << endl;
-    cout << "7) Postorder" << endl;
-    cout << "8) Wyjscie" << endl;
-    cout << "================" << endl;
+void menu() {
+    cout << "\n===== MENU =====\n";
+    cout << "1) Dodaj element\n";
+    cout << "2) Usun element\n";
+    cout << "3) Pokaz sciezke do elementu\n";
+    cout << "4) Preorder\n";
+    cout << "5) Inorder\n";
+    cout << "6) Postorder\n";
+    cout << "7) Zapisz binarnie\n";
+    cout << "8) Wczytaj binarnie\n";
+    cout << "9) Zapisz tekstowo\n";
+    cout << "10) Wczytaj tekstowo\n";
+    cout << "0) Wyjscie\n";
 }
 
 int main() {
-    BST tree;           // Tworzymy obiekt drzewa BST
-    //FileManager fm;     // Tworzymy obiekt FileManager do obsługi plików
-    int choice,key;         // Zmienna do przechowywania wyboru użytkownika
-    
+    BST tree;
+    int choice, key;
+    string filename;
+
     do {
-        printMenu();    // Wyświetlamy menu
-        cout << "Wybierz opcje: ";
+        menu();
+        cout << "Wybor: ";
         cin >> choice;
 
         switch (choice) {
-        case 1: { // Dodawanie elementu
-            cout << "Podaj wartosc do dodania: ";
+
+        case 1:
+            cout << "Podaj wartosc: ";
             cin >> key;
-            tree.insert(key); // Wywołanie metody BST
-			tree.display(); // Wyświetlenie drzewa po dodaniu
+            tree.insert(key);
             break;
-        }
-        case 2: { // Usuwanie elementu
-            cout << "Podaj wartosc do usuniecia: ";
+
+        case 2:
+            cout << "Podaj wartosc: ";
             cin >> key;
-            tree.remove(key); // Wywołanie metody BST
-            tree.display();
+            tree.remove(key);
             break;
-        }
-        case 3: { // Szukanie drogi do elementu
-            cout << "Podaj wartosc do wyszukania: ";
+
+        case 3:
+            cout << "Podaj wartosc: ";
             cin >> key;
-            tree.search(key); // Wywołanie metody BST
+            tree.showPathTo(key);
             break;
-        }
-        case 4: { 
-            if (tree.getRoot()) {
-                cout << "Min:" << tree.minNode(tree.getRoot())->key
-                    << ", Max:" << tree.maxNode(tree.getRoot())->key << endl;
-            }
-			else cout << "Drzewo jest puste." << endl;
-            break; 
-        }
-        case 5: { 
-			cout << "Preorder: "; tree.preorder(tree.getRoot()); cout << endl;
+
+        case 4:
+            cout << "Preorder: ";
+            tree.preorder(tree.getRoot());
+            cout << endl;
             break;
-        }
-        case 6: { 
-			cout << "Inorder: "; tree.inorder(tree.getRoot()); cout << endl;
+
+        case 5:
+            cout << "Inorder: ";
+            tree.inorder(tree.getRoot());
+            cout << endl;
             break;
-        }
-        case 7: { 
-			cout << "Postorder: "; tree.postorder(tree.getRoot()); cout << endl;
+
+        case 6:
+            cout << "Postorder: ";
+            tree.postorder(tree.getRoot());
+            cout << endl;
             break;
-        }
-        case 0: { // Odczyt binarny
-			cout << "Koniec programu." << endl;
+
+        case 7:
+            cout << "Podaj nazwe pliku: ";
+            cin >> filename;
+            FileManager::saveBinary(filename, tree);
             break;
-        }
-        default: { // Niepoprawny wybór
-            cout << "Nieprawidlowy wybor! Sprobuj ponownie." << endl;
+
+        case 8:
+            cout << "Podaj nazwe pliku: ";
+            cin >> filename;
+            FileManager::loadBinary(filename, tree);
             break;
-        }
+
+        case 9:
+            cout << "Podaj nazwe pliku: ";
+            cin >> filename;
+            FileManager::saveText(filename, tree);
+            break;
+
+        case 10:
+            cout << "Podaj nazwe pliku: ";
+            cin >> filename;
+            FileManager::loadText(filename, tree);
+            break;
+
+        case 0:
+            cout << "Koniec programu.\n";
+            break;
+
+        default:
+            cout << "Nieprawidlowy wybor.\n";
         }
 
-    } while (choice != 0); // Pętla działa dopóki użytkownik nie wybierze opcji 0
+    } while (choice != 0);
 
-    return 0; // Zakończenie program
+    return 0;
 }
